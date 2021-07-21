03468aaf98548ed677b5b1c20a4e3379b6af7f47
Fishbone <38041294+leiless@users.noreply.github.com>
2021-07-21 22:53:21 +0800

    download_sqlite.sh: Fix script

--- download_sqlite.sh
+++ download_sqlite.sh
@@ -7,13 +7,14 @@ cd "$(dirname "$0")"
 
 URL=https://www.sqlite.org
 echo "Fetching SQLite3 latest version ..."
-URI="$(curl -4sL "$URL"/download.html | grep -Eo "[0-9]{4}/sqlite-amalgamation-[0-9]+.zip")"
+URI="$(curl -4sL "$URL"/download.html | grep -Eo "[0-9]{4}/sqlite-amalgamation-[0-9]+.zip" | sort -u)"
 
 FILE="$(basename "$URI")"
 DIR="$(basename "$FILE" .zip)"
 echo "Version: $DIR"
 
-wget "$URL/$URI" -O "$FILE"
+ZIP_URL=$URL/$URI
+wget "$ZIP_URL" -O "$FILE"
 
 rm -rf sqlite-amalgamation-*/
 
