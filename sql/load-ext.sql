8749034c64b9858cfac915d660f29be973be9ac6
Fishbone <38041294+leiless@users.noreply.github.com>
2021-10-30 21:13:20 +0800

    sql/load-ext.sql: Add queries

--- sql/load-ext.sql
+++ sql/load-ext.sql
@@ -2,3 +2,7 @@
 CREATE VIRTUAL TABLE t1 USING fts5(x, tokenize = 'ngram_porter gram 2');
 INSERT INTO t1 VALUES(' 2021 年 10 月，在 Ubuntu Linux 上如何使用WeChat ？ ');
 
+-- The following queries all match the single row in the table
+SELECT * FROM t1('Ubuntu Linux');
+SELECT * FROM t1('如何');
+
