c78a795552bf342b94c93cab40504f479b8b857f
Fishbone <38041294+leiless@users.noreply.github.com>
2021-11-06 11:54:01 +0800

    Guard by ngram_tokenizer namespace v2

--- src/token_vector.cpp
+++ src/token_vector.cpp
@@ -5,98 +5,99 @@
 #include <iostream>
 #include <utility>
 
-Token::Token(std::string str, int iStart, int iEnd, token_category_t category) {
-    CHECK_GE(iStart, 0);
-    CHECK_GE(iEnd, 0);
-    CHECK_LT(iStart, iEnd);
+namespace ngram_tokenizer {
+    Token::Token(std::string str, int iStart, int iEnd, token_category_t category) {
+        CHECK_GE(iStart, 0);
+        CHECK_GE(iEnd, 0);
+        CHECK_LT(iStart, iEnd);
 
-    this->str = std::move(str);
-    this->iStart = iStart;
-    this->iEnd = iEnd;
-    this->category = category;
-}
-
-const std::string &Token::get_str() const {
-    return str;
-}
-
-int Token::get_iStart() const {
-    return iStart;
-}
-
-int Token::get_iEnd() const {
-    return iEnd;
-}
-
-token_category_t Token::get_category() const {
-    return category;
-}
-
-TokenVector::TokenVector(const char *pText, int nText) {
-    CHECK_NOTNULL(pText);
-    CHECK_GE(nText, 0);
-    this->pText = pText;
-    this->nText = nText;
-    this->ok = false;
-}
-
-bool TokenVector::tokenize() {
-    int iStart = 0;
-    int iEnd = 0;
-
-    while (iEnd < nText) {
-        token_category_t category = token_category(pText[iEnd]);
-        if (category == OTHER) {
-            int len = utf8_char_count(pText[iEnd]);
-            if (len <= 0) {
-                LOG(ERROR) << "Met non-UTF8 character at index " << iEnd;
-                return false;
-            }
-            iEnd += len;
-        } else {
-            while (++iEnd < nText && token_category(pText[iEnd]) == category) {
-                // continue
-            }
-        }
-
-        if (category != SPACE_OR_CONTROL) {
-            // Will properly null-terminate the resulting std::string
-            std::string s(pText + iStart, iEnd - iStart);
-            tokens.emplace_back(s, iStart, iEnd, category);
-        }
-
-        iStart = iEnd;
+        this->str = std::move(str);
+        this->iStart = iStart;
+        this->iEnd = iEnd;
+        this->category = category;
     }
 
-    if (iEnd != nText) {
-        // Certainly not a valid UTF-8 string
-        return false;
+    const std::string &Token::get_str() const {
+        return str;
     }
-    ok = true;
-    return true;
-}
+
+    int Token::get_iStart() const {
+        return iStart;
+    }
+
+    int Token::get_iEnd() const {
+        return iEnd;
+    }
+
+    token_category_t Token::get_category() const {
+        return category;
+    }
+
+    TokenVector::TokenVector(const char *pText, int nText) {
+        CHECK_NOTNULL(pText);
+        CHECK_GE(nText, 0);
+        this->pText = pText;
+        this->nText = nText;
+        this->ok = false;
+    }
+
+    bool TokenVector::tokenize() {
+        int iStart = 0;
+        int iEnd = 0;
+
+        while (iEnd < nText) {
+            token_category_t category = token_category(pText[iEnd]);
+            if (category == OTHER) {
+                int len = utf8_char_count(pText[iEnd]);
+                if (len <= 0) {
+                    LOG(ERROR) << "Met non-UTF8 character at index " << iEnd;
+                    return false;
+                }
+                iEnd += len;
+            } else {
+                while (++iEnd < nText && token_category(pText[iEnd]) == category) {
+                    // continue
+                }
+            }
+
+            if (category != SPACE_OR_CONTROL) {
+                // Will properly null-terminate the resulting std::string
+                std::string s(pText + iStart, iEnd - iStart);
+                tokens.emplace_back(s, iStart, iEnd, category);
+            }
+
+            iStart = iEnd;
+        }
+
+        if (iEnd != nText) {
+            // Certainly not a valid UTF-8 string
+            return false;
+        }
+        ok = true;
+        return true;
+    }
 
 // Call only after a successful call of tokenize()
-const std::vector<Token> &TokenVector::get_tokens() const {
-    CHECK(ok) << "Make sure tokenize() has been successfully called";
-    return tokens;
-}
+    const std::vector<Token> &TokenVector::get_tokens() const {
+        CHECK(ok) << "Make sure tokenize() has been successfully called";
+        return tokens;
+    }
 
-token_category_t TokenVector::token_category(char c) {
-    if (isdigit(c)) {
-        return DIGIT;
+    token_category_t TokenVector::token_category(char c) {
+        if (isdigit(c)) {
+            return DIGIT;
+        }
+        if (isspace(c) || iscntrl(c)) {
+            return SPACE_OR_CONTROL;
+        }
+        if (isalpha(c)) {
+            return ALPHABETIC;
+        }
+        if (ispunct(c)) {
+            return PUNCTUATION;
+        }
+        return OTHER;
     }
-    if (isspace(c) || iscntrl(c)) {
-        return SPACE_OR_CONTROL;
-    }
-    if (isalpha(c)) {
-        return ALPHABETIC;
-    }
-    if (ispunct(c)) {
-        return PUNCTUATION;
-    }
-    return OTHER;
-}
 
 /**
  * Count how many bytes an UTF-8 character occupied
@@ -110,14 +111,15 @@ token_category_t TokenVector::token_category(char c) {
  *  https://xr.anadoxin.org/source/xref/macos-10.14.1-mojave/xnu-4903.221.2/bsd/vfs/vfs_utfconv.c#639
  *  https://github.com/apple/darwin-xnu/blob/main/bsd/vfs/vfs_utfconv.c#L662
  */
-int TokenVector::utf8_char_count(char c) {
-    int n = 0;
-    while ((c & 0x80) && n < 4) {
-        n++;
-        c <<= 1;
+    int TokenVector::utf8_char_count(char c) {
+        int n = 0;
+        while ((c & 0x80) && n < 4) {
+            n++;
+            c <<= 1;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        return n ? n : 1;
     }
-    if (n == 1) {
-        return 0;
-    }
-    return n ? n : 1;
 }
--- src/token_vector.h
+++ src/token_vector.h
@@ -3,48 +3,50 @@
 #include <string>
 #include <vector>
 
-typedef enum {
-    DIGIT,
-    SPACE_OR_CONTROL,
-    ALPHABETIC,
-    PUNCTUATION,
-    OTHER
-} token_category_t;
+namespace ngram_tokenizer {
+    typedef enum {
+        DIGIT,
+        SPACE_OR_CONTROL,
+        ALPHABETIC,
+        PUNCTUATION,
+        OTHER
+    } token_category_t;
 
-class Token {
-public:
-    Token(std::string, int, int, token_category_t);
+    class Token {
+    public:
+        Token(std::string, int, int, token_category_t);
 
-    const std::string &get_str() const;
+        const std::string &get_str() const;
 
-    int get_iStart() const;
+        int get_iStart() const;
 
-    int get_iEnd() const;
+        int get_iEnd() const;
 
-    token_category_t get_category() const;
+        token_category_t get_category() const;
 
-private:
-    std::string str;
-    int iStart; // Inclusive
-    int iEnd; // Exclusive
-    token_category_t category;
-};
+    private:
+        std::string str;
+        int iStart; // Inclusive
+        int iEnd; // Exclusive
+        token_category_t category;
+    };
 
-class TokenVector {
-public:
-    TokenVector(const char *, int);
+    class TokenVector {
+    public:
+        TokenVector(const char *, int);
 
-    bool tokenize();
+        bool tokenize();
 
-    const std::vector<Token> &get_tokens() const;
+        const std::vector<Token> &get_tokens() const;
 
-private:
-    static token_category_t token_category(char);
+    private:
+        static token_category_t token_category(char);
 
-    static int utf8_char_count(char);
+        static int utf8_char_count(char);
 
-    const char *pText;
-    int nText;
-    std::vector<Token> tokens;
-    bool ok;
-};
+        const char *pText;
+        int nText;
+        std::vector<Token> tokens;
+        bool ok;
+    };
+}
