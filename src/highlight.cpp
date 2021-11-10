e718886acd68c80991aca8e01cc74d52fcc13ef2
Fishbone <38041294+leiless@users.noreply.github.com>
2021-11-10 17:54:24 +0800

    Add fts5HighlightCb()
    Not yet finished

--- src/highlight.cpp
+++ src/highlight.cpp
@@ -8,7 +8,8 @@ SQLITE_EXTENSION_INIT3
 
 // see:
 //  https://github.com/sqlite/sqlite/blob/master/ext/fts5/fts5_aux.c
-//  cad760d16ed403a065dbc90dd5c50f1eb29f5988
+//      cad760d16ed403a065dbc90dd5c50f1eb29f5988
+//  https://github.com/wangfenjin/simple/blob/master/src/simple_highlight.cc#L84
 
 typedef struct CInstIter CInstIter;
 struct CInstIter {
@@ -58,6 +59,11 @@ static inline int fts5CInstIterNext(CInstIter *pIter) {
         }
     }
 
+    if (pIter->iStart >= 0 || pIter->iEnd >= 0) {
+        CHECK(pIter->iStart >= 0 && pIter->iEnd >= 0);
+        DLOG(INFO) << "Output iStart: " << pIter->iStart << " iEnd: " << pIter->iEnd;
+    }
+
     return rc;
 }
 
@@ -89,9 +95,11 @@ static inline int fts5CInstIterInit(
 typedef struct HighlightContext HighlightContext;
 struct HighlightContext {
     CInstIter iter;     /* Coalesced Instance Iterator */
-    int iPos;           /* Current token offset in zIn[] */
+    int iPhrase;        /* Current token offset in zIn[] */
+#if 0
     int iRangeStart;    /* First token to include */
     int iRangeEnd;      /* If non-zero, last token to include */
+#endif
     const char *zOpen;  /* Opening highlight */
     const char *zClose; /* Closing highlight */
     const char *zIn;    /* Input text */
@@ -99,6 +107,41 @@ struct HighlightContext {
     int iOff;           /* Current offset within zIn[] */
     char *zOut;         /* Output value */
 };
+
+/*
+** Tokenizer callback used by implementation of highlight() function.
+*/
+static inline int fts5HighlightCb(
+        void *pContext,                 /* Pointer to HighlightContext object */
+        int tflags,                     /* Mask of FTS5_TOKEN_* flags */
+        const char *pToken,             /* Buffer containing token */
+        int nToken,                     /* Size of token in bytes */
+        int iStartOff,                  /* Start offset of token */
+        int iEndOff                     /* End offset of token */
+) {
+    if (tflags & FTS5_TOKEN_COLOCATED) return SQLITE_OK;
+
+    UNUSED(pToken, nToken);
+    auto p = (HighlightContext *) pContext;
+    // Current token offset(index)
+    int iPhrase = p->iPhrase++;
+
+    if (iPhrase == p->iter.iStart) {
+        DLOG(INFO) << "iStart: " << iStartOff << " " << iEndOff;
+    }
+
+    int rc = SQLITE_OK;
+
+    if (iPhrase == p->iter.iEnd) {
+        if (iPhrase != p->iter.iStart) {
+            DLOG(INFO) << "iEnd: " << iStartOff << " " << iEndOff;
+        }
+
+        rc = fts5CInstIterNext(&p->iter);
+    }
+
+    return rc;
+}
 
 void ngram_highlight(
         const Fts5ExtensionApi *pApi,   /* API offered by current FTS version */
@@ -125,6 +168,11 @@ void ngram_highlight(
     // TODO: do we need to release zIn upon return?
     int rc = pApi->xColumnText(pFts, iCol, &ctx.zIn, &ctx.nIn);
     if (rc == SQLITE_OK && ctx.zIn != nullptr) {
+        // Init the iterator and get the first coalesced phrase
         rc = fts5CInstIterInit(pApi, pFts, iCol, &ctx.iter);
+
+        if (rc == SQLITE_OK) {
+            rc = pApi->xTokenize(pFts, ctx.zIn, ctx.nIn, (void *) &ctx, fts5HighlightCb);
+        }
     }
 }
