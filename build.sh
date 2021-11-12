4ca5ddfdb837c075de74d8d3547d2c93be0cea5c
Fishbone <38041294+leiless@users.noreply.github.com>
2021-11-12 09:26:09 +0800

    Fix build v1

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ if [ ! -L src/sqlite ]; then
     ./download-sqlite.sh
 fi
 
+pushd src/proto
+protoc --cpp_out=. highlight_result.proto
+popd
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --target clean
 cmake --build build
